c13b21352c7d296443e9d2879732a65b1e16e909
Austin Pierce <Austinentpierce@gmail.com>
2021-07-28 16:28:11 -0400

    Done with explorer mode.

--- Program.cs
+++ Program.cs
@@ -87,11 +87,3 @@ namespace AllCardsOnDeckCS
         }
     }
 }
-//for rightIndex from numberOfCards - 1 down to 1 do:
-//leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. See the section "How do we get a random integer")
-
-//Now swap the values at rightIndex and leftIndex by doing this:
-//leftCard = the value from deck[leftIndex]
-//rightCard = the value from deck[rightIndex]
-//deck[rightIndex] = leftCard
-//deck[leftIndex] = rightCard 
